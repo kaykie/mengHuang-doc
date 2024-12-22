5b0512c56fa59c8ad26909742b760c729d933ca5
kaykie <kaykie0421@163.com>
2024-12-22 21:24:30 +0800

    feat 修改icon

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -6,12 +6,12 @@ import { notes } from './notes'
  * @see https://theme-plume.vuejs.press/config/basic/
  */
 export default defineThemeConfig({
-  logo: 'https://theme-plume.vuejs.press/plume.png',
+  logo: 'https://joke-gif.oss-cn-beijing.aliyuncs.com/icon.png?Expires=1734874108&OSSAccessKeyId=TMP.3KijPty5Jf2ygc29LVmHj1hcbkCBL7MJSX6Tn6upJho2cJeaJCdmj8SRRmdd4RMuL3U1mtbrWVXKw3RkGXojo4VtVCFSLX&Signature=cWU0q%2FmtrX7BHPo5KbjP34CAHIo%3D',
 
   appearance: true,
 
   profile: {
-    avatar: 'https://theme-plume.vuejs.press/plume.png',
+    avatar: 'https://joke-gif.oss-cn-beijing.aliyuncs.com/icon.png?Expires=1734874108&OSSAccessKeyId=TMP.3KijPty5Jf2ygc29LVmHj1hcbkCBL7MJSX6Tn6upJho2cJeaJCdmj8SRRmdd4RMuL3U1mtbrWVXKw3RkGXojo4VtVCFSLX&Signature=cWU0q%2FmtrX7BHPo5KbjP34CAHIo%3D',
     name: '悠然助手',
     description: '',
     layout:'right'
