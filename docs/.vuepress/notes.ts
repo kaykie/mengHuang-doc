bcb1097625f2e97e67238713efe48d2db8e7a742
kaykie <kaykie0421@163.com>
2024-12-30 10:04:04 +0800

    feat 日志提交

--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -19,45 +19,52 @@ const logNote = defineNoteConfig({
   dir: '/',
   link: '/',
   sidebar: [
-    {text:'开始',prefix:'/help',items:[
-      {
-        text:'开始使用',
-        link:'/help/start/'
-      },
-      {
-        text:'使用虚拟机',
-        link:'/help/xejh8sxr/'
-      },
-      {
-        text:'一键日常',
-        link:'/help/ga7ah6kh/'
-      },
-      {
-        text:'自动副本功能',
-        link:'/help/69y9lhj6/'
-      },
-      {
-        text:'自动抓鬼功能',
-        link:'/help/tb561b7g/'
-      },
-      {
-        text:'低价秒非珍',
-        link:'/help/q62639g4/'
-      },
-      {
-        text:'周一自动门派',
-        link:'/help/pm1sbw4v/'
-      },
-      {
-        text:'自动三界、科举',
-        link:'/help/50o8buc4/'
-      },
-      {
-        text:'新区抢铅',
-        link:'/help/uum1yqlg/'
-      },
+    {
+      text:'开始使用',link:'/help/all-start/',
       
-    ]},
+    },
+    {
+      text:'梦幻西游手游',prefix:'/menghuan/',collapsed:true,
+      items:[
+        {
+          text:'开始使用',link:'/help/start/',
+          
+        },
+        {
+          text:'使用虚拟机',
+          link:'/help/xejh8sxr/'
+        },
+        {
+          text:'一键日常',
+          link:'/help/ga7ah6kh/'
+        },
+        {
+          text:'自动副本功能',
+          link:'/help/69y9lhj6/'
+        },
+        {
+          text:'自动抓鬼功能',
+          link:'/help/tb561b7g/'
+        },
+        {
+          text:'低价秒非珍',
+          link:'/help/q62639g4/'
+        },
+        {
+          text:'周一自动门派',
+          link:'/help/pm1sbw4v/'
+        },
+        {
+          text:'自动三界、科举',
+          link:'/help/50o8buc4/'
+        },
+        {
+          text:'新区抢铅',
+          link:'/help/uum1yqlg/'
+        },
+        
+      ]
+    },
     {text:'更新日志',prefix:'/logs',items:[
       {
         text:'2024-12-22',
@@ -66,8 +73,29 @@ const logNote = defineNoteConfig({
       {
         text:'2024-12-24',
         link:'/logs/mzwe5j3g/'
+      },
+      {
+        text:'2024-12-25',
+        link:'/smmb2xmm/'
+      },
+      {
+        text:'2024-12-27',
+        link:'/pur25c8o/'
+      },
+      {
+        text:'2024-12-30',
+        link:'/dg1w8up5/'
       }
-    ]}
+    ]},
+    {
+      text:'签到助手',prefix:'/autoClock/',collapsed:true,
+      items:[
+        {
+          text:'开始使用',link:'/autoClock/start/',
+        },
+      ]
+    },
+    
   ]
 })
 
